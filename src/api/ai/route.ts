a7e817345859af1446b17454b137e79c875ba7d7
Deibson Cogo <deibsoncogo@outlook.com>
2025-04-29 22:30:00 -0300

    feat: implementação do open router

--- src/api/ai/route.ts
+++ src/api/ai/route.ts
@@ -1,10 +1,10 @@
-import { openai } from "@ai-sdk/openai";
 import { generateText } from "ai";
 import { type NextRequest, NextResponse } from "next/server";
+import { openRouter } from "./open-router";
 
 export async function GET(request: NextRequest) {
 	const result = await generateText({
-		model: openai("gpt-4o"),
+		model: openRouter.chat("openai/chatgpt-4o-latest"),
 		prompt: 'Traduza "Hello World" para português!',
 		system:
 			"Você é uma AI especializada em tradução, sempre retorno da maneiro mais sucinta",
