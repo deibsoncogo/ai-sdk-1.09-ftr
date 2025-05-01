907db808f1e8a73c0a02cbda1b59d8b2e4afc0fe
Deibson Cogo <deibsoncogo@outlook.com>
2025-04-30 21:15:30 -0300

    feat: mudando o tipo de respostas para um objeto

--- src/api/ai/route.ts
+++ src/api/ai/route.ts
@@ -1,14 +1,19 @@
-import { generateText } from "ai";
+import { generateObject } from "ai";
 import { type NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
 import { openRouter } from "./open-router";
 
 export async function GET(request: NextRequest) {
-	const result = await generateText({
-		model: openRouter.chat("openai/chatgpt-4o-latest"),
-		prompt: 'Traduza "Hello World" para português!',
-		system:
-			"Você é uma AI especializada em tradução, sempre retorno da maneiro mais sucinta",
+	const result = await generateObject({
+		model: openRouter.chat("openai/gpt-4o-2024-11-20"),
+		schema: z.object({
+			en: z.string().describe("Tradução para inglês"),
+			fr: z.string().describe("Tradução para francês"),
+			es: z.string().describe("Tradução para espanhol"),
+		}),
+		prompt: 'Traduza "Hello World" para diferentes idiomas!',
+		system: "Você é uma AI especializada em tradução",
 	});
 
-	return NextResponse.json({ message: result.text });
+	return NextResponse.json({ message: result.object });
 }
